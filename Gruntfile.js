73e2f412d4f6d1813b9c867eaf784e78f02b30c2
Lynn Fisher <lynnfishe@gmail.com>
2019-03-04 07:09:44 -0700

    add redirect

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,6 +77,10 @@ module.exports = function(grunt) {
                         expand: true,
                         src: ['**'],
                         dest: 'public/assets'
+                    },
+                    {
+                        src: ['_redirects'],
+                        dest: 'public/'
                     }
                 ]
             },
